af958fe3377976da3c913c3acbc09653b3e7a901
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2022-04-08 17:09:09 +0200

    Add a basic router

---
+++ pkg/router/router.go
@@ -0,0 +1,21 @@
+package router
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+// CreateRouter returns creates a new chi router
+func CreateRouter() *chi.Mux {
+	r := chi.NewRouter()
+
+	r.Use(middleware.Logger)
+
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Frontpad"))
+	})
+
+	return r
+}
