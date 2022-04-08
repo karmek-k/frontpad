60259a0df6a1b4d55ce6728a7d204b9e7b551abe
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2022-04-08 17:09:37 +0200

    Load `.env` contents and serve routes

--- cmd/frontpad/main.go
+++ cmd/frontpad/main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/karmek-k/frontpad/pkg/router"
+)
 
 func main() {
-	fmt.Println("Frontpad")
+	err := godotenv.Load()
+	if err != nil {
+		panic("no .env file found")
+	}
+
+	r := router.CreateRouter()
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8000"
+	}
+
+	http.ListenAndServe(":"+port, r)
 }
