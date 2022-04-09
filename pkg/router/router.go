ea7cdfc8f0c49d614a50460bbc17a99c37a89bd5
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2022-04-09 12:59:09 +0200

    Create session routes

--- pkg/router/router.go
+++ pkg/router/router.go
@@ -1,21 +1,26 @@
 package router
 
 import (
-	"context"
-
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 	"github.com/go-chi/render"
+	"github.com/karmek-k/frontpad/pkg/router/routes"
 )
-
-var ctx = context.Background()
 
 // CreateRouter creates a new chi router
 func CreateRouter() *chi.Mux {
 	r := chi.NewRouter()
 
 	r.Use(middleware.Logger)
-	r.Use(render.SetContentType(render.ContentTypeJSON))
+	
+	r.Route("/api", func(r chi.Router) {
+		r.Use(render.SetContentType(render.ContentTypeJSON))
+
+		r.Route("/session", func(r chi.Router) {
+			r.Post("/", routes.SessionCreate)
+			r.Delete("/{id}", routes.SessionDelete)
+		})
+	})
 
 	return r
 }
---
+++ pkg/router/routes/session.go
@@ -0,0 +1,11 @@
+package routes
+
+import "net/http"
+
+func SessionCreate(w http.ResponseWriter, r *http.Request) {
+	
+}
+
+func SessionDelete(w http.ResponseWriter, r *http.Request) {
+
+}
