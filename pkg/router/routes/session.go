92fd0e5adf61d24d3d4465614ae2402f031d23c7
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2022-04-10 15:09:41 +0200

    Add `SessionDelete`

--- pkg/router/routes/session.go
+++ pkg/router/routes/session.go
@@ -3,7 +3,9 @@ package routes
 import (
 	"net/http"
 
+	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/render"
+	"github.com/go-redis/redis/v8"
 	"github.com/karmek-k/frontpad/pkg/db"
 	"github.com/karmek-k/frontpad/pkg/db/models"
 	"github.com/karmek-k/frontpad/pkg/utils"
@@ -27,9 +29,9 @@ func SessionCreate(w http.ResponseWriter, r *http.Request) {
 	}
 	
 	// save it to redis
-	_, err = db.RDB.HSet(r.Context(), session.Id,
+	err = db.RDB.HSet(r.Context(), session.Id,
 		"password", session.PasswordHashed,
-	).Result()
+	).Err()
 	if err != redis.Nil {
 		panic(err)
 	}
@@ -39,5 +41,26 @@ func SessionCreate(w http.ResponseWriter, r *http.Request) {
 }
 
 func SessionDelete(w http.ResponseWriter, r *http.Request) {
+	// find session in redis
+	id := chi.URLParam(r, "id")
+	hashed, err := db.RDB.HGet(r.Context(), id, "password").Result()
 
+	if err == redis.Nil {
+		w.WriteHeader(http.StatusNotFound)
+
+		return
+	}
+
+	// compare passwords
+	matching := utils.CheckPassword("12345", hashed)
+	if !matching {
+		w.WriteHeader(http.StatusForbidden)
+
+		return
+	}
+	
+	// delete the session
+	_ = db.RDB.HDel(r.Context(), id).Err()
+
+	w.WriteHeader(http.StatusNoContent)
 }
