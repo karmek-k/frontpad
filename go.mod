28c671901cc1c3d7f010ebb7b14461598326b002
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2022-04-11 18:59:34 +0200

    Install CORS middleware

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.18
 
 require (
 	github.com/go-chi/chi/v5 v5.0.7
+	github.com/go-chi/cors v1.2.0
 	github.com/go-redis/redis/v8 v8.11.5
 	github.com/joho/godotenv v1.4.0
 	github.com/satori/go.uuid v1.2.0
--- pkg/router/router.go
+++ pkg/router/router.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
 	"github.com/karmek-k/frontpad/pkg/router/routes"
 	"github.com/karmek-k/frontpad/pkg/websocket"
 )
@@ -16,6 +17,7 @@ func CreateRouter() *chi.Mux {
 
 	r.Use(middleware.Logger)
 	r.Use(middleware.Recoverer)
+	r.Use(cors.AllowAll().Handler) // TODO: change when deploying
 	
 	r.Route("/api", func(r chi.Router) {
 		r.Route("/session", func(r chi.Router) {
