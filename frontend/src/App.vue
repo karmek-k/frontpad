849444499dc672a1600053ea1e987b3d301bb026
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2022-04-16 20:02:58 +0200

    Move some logic from ConnectionForm to App

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,12 +2,85 @@
 import AppHeader from './components/AppHeader.vue';
 import ConnectionForm from './components/ConnectionForm.vue';
 import CodeEditors from './components/CodeEditors.vue';
+import { reactive } from 'vue';
 
-let ws = null;
+const state = reactive({
+  ws: null,
+  isError: false,
+  sessionId: '',
+  isLoading: false
+});
+
+async function createNewSession(apiUrl) {
+  const res = await fetch(apiUrl, {
+    method: 'POST'
+  });
+  const json = await res.json();
+
+  return json.id;
+}
+
+function handleNewSession() {
+  state.isError = false;
+  state.isLoading = true;
+
+  createNewSession('http://localhost:8000/api/session')
+    .then(id => (state.sessionId = id))
+    .catch(() => (state.isError = true))
+    .finally(() => (state.isLoading = false));
+}
+
+function handleConnectWs() {
+  state.ws = connectWs('ws://localhost:8000/ws');
+}
+
+function handleDisconnectWs() {
+  disconnectWs(false);
+}
+
+function connectWs(url) {
+  if (state.ws !== null) {
+    return null;
+  }
+
+  const ws = new WebSocket(url);
+  ws.addEventListener('error', () => {
+    state.isError = true;
+  });
+
+  ws.addEventListener('open', () => {
+    state.isError = false;
+  });
+
+  ws.addEventListener('close', () => {
+    disconnectWs(true);
+  });
+
+  return ws;
+}
+
+function disconnectWs(fromWsCloseEvent) {
+  if (state.ws === null) {
+    return;
+  }
+
+  if (!fromWsCloseEvent) {
+    state.ws.close();
+  }
+
+  state.ws = null;
+}
 </script>
 
 <template>
   <AppHeader />
-  <ConnectionForm />
-  <CodeEditors v-show="ws !== null" />
+  <ConnectionForm
+    :is-error="state.isError"
+    :is-connected="isConnected"
+    :disable-create-session="state.sessionId.length > 0"
+    @session-create="handleNewSession"
+    @ws-connect="handleConnectWs"
+    @ws-disconnect="handleDisconnectWs"
+  />
+  <CodeEditors v-show="isConnected" />
 </template>
--- frontend/src/components/ConnectionForm.vue
+++ frontend/src/components/ConnectionForm.vue
@@ -1,82 +1,49 @@
 <script setup>
 import { computed, reactive } from 'vue';
 
+defineEmits(['sessionCreate', 'wsConnect', 'wsDisconnect']);
+
+const props = defineProps({
+  isConnected: {
+    type: Boolean
+  },
+  isError: {
+    type: Boolean
+  },
+  disableCreateSession: {
+    type: Boolean,
+    default: false
+  }
+});
+
 const state = reactive({
-  sessionId: '',
-  isError: false,
-  ws: null
+  inputText: ''
 });
-
-const inputHasText = computed(() => {
-  return state.sessionId.length > 0;
-});
-
-const isConnected = computed(() => {
-  return state.ws !== null;
-});
-
-async function newSession() {
-  const res = await fetch('http://localhost:8000/api/session', {
-    method: 'POST'
-  });
-  const json = await res.json();
-
-  return json.id;
-}
-
-function connectWs(url) {
-  if (!inputHasText && isConnected) {
-    return null;
-  }
-
-  const ws = new WebSocket(url);
-  ws.addEventListener('error', () => {
-    state.isError = true;
-  });
-
-  ws.addEventListener('open', () => {
-    state.isError = false;
-  });
-
-  return ws;
-}
-
-function disconnectWs() {
-  if (!isConnected) {
-    return;
-  }
-
-  state.ws.close();
-  state.ws = null;
-}
 </script>
 
 <template>
-  <p v-show="state.isError">An error has occurred</p>
+  <p v-show="props.isError">An error has occurred</p>
   <input
     type="text"
-    v-model="state.sessionId"
+    v-model="state.inputText"
     placeholder="Leave empty to create"
-    :disabled="isConnected"
+    :disabled="props.isConnected"
   />
   <button
-    @click="
-      () =>
-        newSession()
-          .then(id => (state.sessionId = id))
-          .catch(() => (state.isError = true))
-    "
-    :disabled="inputHasText"
+    @click="$emit('sessionCreate')"
+    :disabled="props.disableCreateSession"
   >
     Create a session
   </button>
   <button
-    @click="() => (state.ws = connectWs('ws://localhost:8000/ws'))"
-    :disabled="isConnected || !inputHasText"
+    @click="$emit('wsConnect')"
+    :disabled="props.isConnected || !props.disableCreateSession"
   >
     Connect
   </button>
-  <button @click="disconnectWs" :disabled="!isConnected">Disconnect</button>
+  <button @click="$emit('wsDisconnect')" :disabled="!props.isConnected">
+    Disconnect
+  </button>
 </template>
 
 <style scoped>
